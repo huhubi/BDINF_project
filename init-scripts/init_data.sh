dd70bc3d25d6cbedb889bef6b4cc871fb07d7e81
vermout94 <david.berger4494@gmail.com>
2024-05-01 16:05:00 +0200

    Final project files

--- init-scripts/init_data.sh
+++ init-scripts/init_data.sh
@@ -19,16 +19,17 @@ echo "MongoDB is ready!" >> $log_file
 echo "Starting data import..." >> $log_file
 
 # Import emissions data
-mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection emissions --type csv --file /emissions/schadstoffemissionen_1990-2022_nach_nfr_wide_1a3b1.csv --headerline --columnsHaveTypes --delimiter ';' --fieldFile /emissions/fields.txt >> $log_file 2>&1
+mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection emissions --type csv --file /data/emissions/schadstoffemissionen_1990-2022_nach_nfr_wide_1a3b1.csv --columnsHaveTypes --fieldFile /data/emissions/fields.txt >> $log_file 2>&1
 echo "Emissions data import complete." >> $log_file
 
 # Import new cars data
-mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection newcars --type csv --file /newcars/overalldata_new.csv --headerline --columnsHaveTypes --delimiter ';' --fieldFile /newcars/fields.txt >> $log_file 2>&1
+mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection newcars --type csv --file /data/newcars/overalldata_new.csv --columnsHaveTypes  --fieldFile /data/newcars/fields.txt >> $log_file 2>&1
 echo "New cars data import complete." >> $log_file
 
 # Import used cars data
-mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection usedcars --type csv --file /usedcars/overalldata_used.csv --headerline --columnsHaveTypes --delimiter ';' --fieldFile /usedcars/fields.txt >> $log_file 2>&1
+mongoimport --username root --password example --authenticationDatabase admin --db mydatabase --collection usedcars --type csv --file /data/usedcars/overalldata_used.csv --columnsHaveTypes  --fieldFile /data/usedcars/fields.txt >> $log_file 2>&1
 echo "Used cars data import complete." >> $log_file
+
 
 echo "All data imports are done." >> $log_file
 
